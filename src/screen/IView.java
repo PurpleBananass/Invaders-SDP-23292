94cfcefc6cc328eaf4de7c4725cbfd81e488f4e4
Sognare <90079601+BBAAMM@users.noreply.github.com>
2025-11-04 23:38:39 +0900

    Refactor: remove constants from interface

--- src/screen/IView.java
+++ src/screen/IView.java
@@ -5,18 +5,6 @@ import engine.Core;
 import java.util.logging.Logger;
 
 public interface IView {
-
-	/** Milliseconds until the screen accepts user input. */
-	int INPUT_DELAY = 1000;
-	/** Screen width. */
-	int VIEW_WIDTH = 800;
-	/** Screen height. */
-	int VIEW_HEIGHT = 1000;
-	/** Frames per second shown on the screen. */
-	int FPS = 60;
-	/** Application logger. */
-	Logger logger = Core.getLogger();
-
 	/** Load assets from outside ( fonts/images/sounds etc. ) */
 	void loadAssets(Object ... assets);
 	/** Draws the achievements on the screen */
@@ -27,8 +15,8 @@ public interface IView {
 	default void onHide() {}
 	/** Dispose resources */
 	void dispose();
-	/** Get screen width */
-	default int getViewWidth(){return VIEW_WIDTH;}
-	/** Get screen height */
-	default int getViewHeight(){return VIEW_HEIGHT;}
+	/** Get view's width */
+	int getWidth();
+	/** Get view's height */
+	int getHeight();
 }
