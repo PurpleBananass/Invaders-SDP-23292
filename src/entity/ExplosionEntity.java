a26604226780623a94e71e8c3124a2058d7c8c3a
아마르척트 <75429961+Amraa-gif@users.noreply.github.com>
2025-11-10 01:45:25 +0900

    Create ExplosionEntity.java

---
+++ src/entity/ExplosionEntity.java
@@ -0,0 +1,44 @@
+package entity;
+
+import java.awt.Color;
+import java.util.Random;
+import engine.DrawManager;
+
+public class ExplosionEntity extends Entity {
+
+    private static final long DURATION = 300; // 0.3 секунд
+
+    private long startTime;
+
+   private boolean isFinished = false;
+
+    private static final Random random = new Random();
+
+    public ExplosionEntity(final int x, final int y) {
+        super(x, y); 
+        
+        this.spriteType = DrawManager.SpriteType.Explosion;
+        
+        int colorType = random.nextInt(3);
+        if (colorType == 0) {
+            this.setColor(Color.ORANGE);
+        } else if (colorType == 1) {
+            this.setColor(Color.YELLOW);
+        } else {
+            this.setColor(Color.RED);
+        }
+
+        this.startTime = System.currentTimeMillis();
+    }
+
+    public void update() {
+        long now = System.currentTimeMillis();
+        if (now - this.startTime > DURATION) {
+            this.isFinished = true;
+        }
+    }
+
+    public boolean isFinished() {
+        return this.isFinished;
+    }
+}
