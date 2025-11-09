0638a0009108cc681b680328eddd486ed44aa694
아마르척트 <75429961+Amraa-gif@users.noreply.github.com>
2025-11-10 01:47:21 +0900

    Create EffectManager.java

---
+++ src/engine/EffectManager.java
@@ -0,0 +1,50 @@
+package engine;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Iterator;
+import entity.ExplosionEntity; // Шинэ класс-аа import хийнэ
+
+public class EffectManager {
+
+    private static EffectManager instance;
+    
+    private List<ExplosionEntity> explosions;
+  
+    private EffectManager() {
+        explosions = new ArrayList<>();
+    }
+
+    public static EffectManager getInstance() {
+        if (instance == null) {
+            instance = new EffectManager();
+        }
+        return instance;
+    }
+
+    public void createExplosion(final int x, final int y) {
+        ExplosionEntity effect = new ExplosionEntity(x, y);
+        this.explosions.add(effect);
+    }
+
+    public void update() {
+        Iterator<ExplosionEntity> iter = this.explosions.iterator();
+        while (iter.hasNext()) {
+            ExplosionEntity effect = iter.next();
+            effect.update();
+            
+            if (effect.isFinished()) {
+                iter.remove();
+            }
+        }
+    }
+
+    public List<ExplosionEntity> getEffects() {
+        return this.explosions;
+    }
+
+    public void clearEffects() {
+        this.explosions.clear();
+    }
+}
